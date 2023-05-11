68fd7189dd5b1ba596edf7e6d6afeb38c232fe1a
ffatique <f_fatique@hotmail.com>
2023-05-10 21:54:23 -0300

    set standard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,6 @@
 import Head from 'next/head'
 import styles from '../styles/home.module.scss'
 import Image from 'next/image'
-import foto from '../../public/images/foto.png'
-import imagem1 from '../../public/images/imagem1.png'
-import imagem2 from '../../public/images/imagem2.png'
-import imagem3 from '../../public/images/imagem3.png'
-import imagem4 from '../../public/images/imagem4.png'
-import imagem5 from '../../public/images/imagem5.png'
 import ens from '../../public/images/ens.png'
 import logo from '../../public/images/logo.png'
 import lens from '../../public/images/lens.png'
@@ -15,14 +9,48 @@ import Link from 'next/link'
 import { CiTwitter, CiYoutube, CiInstagram, CiLinkedin, CiMail } from 'react-icons/ci'
 import { BsShare } from 'react-icons/bs'
 import { RWebShare } from "react-web-share"
+import { VscGithub } from 'react-icons/vsc'
+import { GetServerSideProps } from 'next'
 
 
-export default function Home(){
+interface Props {
+  bmode: string,
+  bname: string,
+  btitle: string,
+  bens: string,
+  blinkOne: string,
+  blinkTwo: string,
+  blinkThree: string,
+  blinkFour: string,
+  blinkFive: string,
+  btitleOne: string,
+  btitleTwo: string,
+  btitleThree: string,
+  btitleFour: string,
+  btitleFive: string,
+  blens: string,
+  btwitter: string,
+  byoutube: string,
+  binstagram: string,
+  blinkedin: string,
+  bgithub: string,
+  bemail: string,
+  bavatarUrl: string,
+  bavatarOne: string,
+  bavatarTwo: string,
+  bavatarThree: string,
+  bavatarFour: string,
+  bavatarFive: string
+}
+
+
+export default function Home({bmode, bname, btitle, bens, blinkOne, blinkTwo, blinkThree, blinkFour, blinkFive, btitleOne, btitleTwo, btitleThree, btitleFour,
+  btitleFive, blens, btwitter, byoutube, binstagram, blinkedin, bgithub, bemail, bavatarUrl, bavatarOne, bavatarTwo, bavatarThree, bavatarFour, bavatarFive}: Props){
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>Linkeh | Vinícius Bedum</title>
+        <title>Linkeh | {bname}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta content="#515AFF" name="theme-color"></meta>
       </Head>
@@ -31,9 +59,9 @@ export default function Home(){
         
         <RWebShare
           data={{
-            text: "Link | Vinicíus Bedum",
+            text: `Linkeh | ${bname}`,
             url: "",
-            title: "Link | Vinicíus Bedum",
+            title: `Linkeh | ${bname}`,
           }}
           onClick={() => console.log("Shared Successfully!")}
         >
@@ -48,89 +76,181 @@ export default function Home(){
 
       <main className={styles.mainContainer}>
 
-        <Image src={foto} alt="Foto Vinícius Bedum" />
+        <Image src={bavatarUrl} width={120} height={120} alt={`Foto ${bname}`} />
 
-        <h2>Vinícius Bedum</h2>
-        <h3>Explico Web3 e NFTs em sua essência.</h3>
+        <h2>{bname}</h2>
+        <h3>{btitle}</h3>
         <div className={styles.ensContainer}>
           <Image src={ens} alt="Logo ENS" />
-          <p>viniciusbedum.eth</p>
+          <p>{bens}</p>
         </div>
         
-        <h4>iBEED</h4>
-
-        <div className={styles.buttons}>
-          <Link target="_blank" href="https://ibeed.xyz/">
-            <button><Image src={imagem1} alt="link imagem 1"/>
-              <p>Site</p>
-            </button>
-          </Link>
-        </div>
+        {blinkOne == null?
+          <></>
+        : 
+          <div className={styles.buttons}>
+            <Link target="_blank" href={blinkOne}>
+              <button><Image src={bavatarOne} width={40} height={40} alt={'Logo Linkeh One'}/>
+                <p>{btitleOne}</p>
+              </button>
+            </Link>
+          </div>
+        }
         
-        <div className={styles.buttons}>
-          <Link target="_blank" href="https://ibeed.xyz/galeria/">
-            <button><Image src={imagem2} alt="link imagem 2"/>
-              <p>Aprenda Sobre Web3 e NFTs</p>
-            </button>
-          </Link>
-        </div>
+        {blinkTwo == null?
+          <></>
+        : 
+          <div className={styles.buttons}>
+            <Link target="_blank" href={blinkTwo}>
+              <button><Image src={bavatarTwo} width={40} height={40} alt={'Logo Linkeh Two'}/>
+                <p>{btitleTwo}</p>
+              </button>
+            </Link>
+          </div>
+        }
 
-        <div className={styles.buttons}>
-           <Link target="_blank" href="https://ibeed.xyz/mint-ibeed-identidade/">
-            <button><Image src={imagem3} alt="link imagem 3"/>
-              <p>Resgate seu iBEED Identidade (NFT)</p>
-            </button>
-          </Link>
-        </div>
+        {blinkThree == null?
+          <></>
+        : 
+          <div className={styles.buttons}>
+            <Link target="_blank" href={blinkThree}>
+              <button><Image src={bavatarThree} width={40} height={40} alt={'Logo Linkeh Three'}/>
+                <p>{btitleThree}</p>
+              </button>
+            </Link>
+          </div>
+        }
 
+        {blinkFour== null?
+          <></>
+        : 
+          <div className={styles.buttons}>
+            <Link target="_blank" href={blinkFour}>
+              <button><Image src={bavatarFour} width={40} height={40} alt={'Logo Linkeh Four'}/>
+                <p>{btitleFour}</p>
+              </button>
+            </Link>
+          </div>
+       }
 
-        <div className={styles.buttons}>
-          <Link target="_blank" href="https://discord.com/invite/2jhsuJvqPd">
-            <button><Image src={imagem4} alt="link imagem 4"/>
-              <p>Seja membro da nossa comunidade</p>
-            </button>
-          </Link>
-        </div>
-
-        <p>iBEED Studios</p>
-
-        <div className={styles.buttons}>
-          <Link target="_blank" href="https://ibeed.xyz/studios">
-            <button><Image src={imagem5} alt="link imagem 5" />
-              <p>Ajudamos sua marca entrar na Web3</p>
-            </button>
-          </Link>
-        </div>
+        {blinkFive == null?
+          <></>
+        : 
+          <div className={styles.buttons}>
+            <Link target="_blank" href={blinkFive}>
+              <button><Image src={bavatarFive} width={40} height={40} alt={'Logo Linkeh Five'}/>
+                <p>{btitleFive}</p>
+              </button>
+            </Link>
+          </div>
+        }
 
       </main>
 
       <div className={styles.footerContainer}>
-        <Link target="_blank" href="https://twitter.com/viniciusbedum">
-          <CiTwitter size={35} color="#d1d1d1d1" />
-        </Link>
-        <Link target="_blank" href="https://www.youtube.com/channel/UCKzh65h2cAt9PO68UeGznJQ">
-          <CiYoutube size={35} color="#d1d1d1d1" />
-        </Link>
-        <Link target="_blank" href="https://www.instagram.com/viniciusbedum/">
-          <CiInstagram size={32} color="#d1d1d1d1" />
-        </Link>
-        <Link target="_blank" href="https://www.linkedin.com/in/vin%C3%ADcius-bedum-060336186/">
-          <CiLinkedin size={33} color="#d1d1d1d1" />
-        </Link>
-        <Link target="_blank" href="mailto:viniciusbedum@ibeed.xyz">
-          <CiMail size={35} color="#d1d1d1d1" />
-        </Link>
-        <Link target="_blank" href="https://lenster.xyz/u/viniciusbedum">
-          <Image src={lens} alt="Logo Lens" />
-        </Link>
+        
+        {btwitter == null?
+          <></>
+        :
+          <Link target="_blank" href={btwitter}>
+            <CiTwitter size={35} color="#d1d1d1d1" />
+          </Link>
+        }
+
+        {byoutube== null?
+          <></>
+        :
+          <Link target="_blank" href={byoutube}>
+            <CiYoutube size={35} color="#d1d1d1d1" />
+          </Link>
+        }
+
+        {binstagram == null?
+          <></>
+        :
+          <Link target="_blank" href={binstagram}>
+            <CiInstagram size={32} color="#d1d1d1d1" />
+          </Link>
+        }
+
+        {blinkedin == null?
+          <></>
+        :
+          <Link target="_blank" href={blinkedin}>
+            <CiLinkedin size={33} color="#d1d1d1d1" />
+          </Link>
+        }
+
+        {bgithub == null?
+          <></>
+        :
+          <Link target="_blank" href={bgithub}>
+            <VscGithub size={33} color="#d1d1d1d1" />
+          </Link>
+        }
+        
+        {bemail == null?
+          <></>
+        :
+          <Link target="_blank" href={bemail}>
+            <CiMail size={35} color="#d1d1d1d1" />
+          </Link>
+        }
+        
+        {blens == null?
+          <></>
+        :
+          <Link target="_blank" href={blens}>
+            <Image src={lens} alt="Logo Lens" />
+          </Link>
+        }
+
       </div>
 
       <div className={styles.linkehContainer}>
-        <Link target="_blank" href="https://twitter.com/viniciusbedum">
+        <Link target="_blank" href="https://www.linkeh.xyz/">
           <Image src={logo} alt="Logo Linkeh" />
         </Link>
       </div>    
     </div>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  
+  const { bmode, bname, btitle, bens, blinkOne, blinkTwo, blinkThree, blinkFour, blinkFive, btitleOne, btitleTwo, btitleThree, btitleFour,
+    btitleFive, blens, btwitter, byoutube, binstagram, blinkedin, bgithub, bemail, bavatarUrl, bavatarOne, bavatarTwo, bavatarThree, bavatarFour, bavatarFive } = context.query;
+
+  return {
+    props: {
+      bmode,
+      bname,
+      btitle,
+      bens,
+      blinkOne,
+      blinkTwo,
+      blinkThree,
+      blinkFour,
+      blinkFive,
+      btitleOne,
+      btitleTwo,
+      btitleThree,
+      btitleFour,
+      btitleFive,
+      blens,
+      btwitter,
+      byoutube,
+      binstagram,
+      blinkedin,
+      bgithub,
+      bemail,
+      bavatarUrl,
+      bavatarOne,
+      bavatarTwo,
+      bavatarThree,
+      bavatarFour,
+      bavatarFive
+    },
+  };
+};
 
